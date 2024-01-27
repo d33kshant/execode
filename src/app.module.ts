f93ada6b2d482a7282473c7c91473eba90144465
Deekshant Yadav <d33kshant@gmail.com>
2024-01-27 11:40:08 +0530

    feat: created queue module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExecModule } from './exec/exec.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [ExecModule],
+  imports: [ExecModule, QueueModule],
   controllers: [AppController],
   providers: [AppService],
 })
