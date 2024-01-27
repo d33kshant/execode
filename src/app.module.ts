b66ade47ff36e43dc3166445e31ba8ee96a3c070
Deekshant Yadav <d33kshant@gmail.com>
2024-01-27 15:38:14 +0530

    fear: added bull config for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExecModule } from './exec/exec.module';
 import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [ExecModule, QueueModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        }
+      }),
+      inject: [ConfigService],
+    }),
+    BullModule.registerQueue({
+      name: 'codes',
+    }),
+    ExecModule,
+    QueueModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
