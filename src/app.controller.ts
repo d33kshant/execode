5f2d6e5fcb929c15b6e7f5003260ef262f777c61
Deekshant Yadav <d33kshant@gmail.com>
2024-02-11 23:46:46 +0530

    feat: created post route for the api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async execute(@Body('source') source: string) {
+    const result = await this.appService.execute(source);
+    return result;
   }
 }
