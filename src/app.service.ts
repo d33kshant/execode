f88504171817735a203edd05f3d3da3a31355534
Deekshant Yadav <d33kshant@gmail.com>
2024-02-11 23:47:38 +0530

    feat: added logic to enqueue jobs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Job, Queue } from 'bull';
+import { QUEUE } from './constants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectQueue(QUEUE) private sourceQueue: Queue) { }
+
+  async execute(source: string) {
+    const job: Job<string> = await this.sourceQueue.add({ source });
+    return job.finished();
   }
 }
